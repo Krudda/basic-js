254b529686348115ddd6bcf52e302c7c0fe3e654
Dmitriy <krudda@yandex.ru>
2020-02-28 14:03:33 +0300

    feat: vigenere complete

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,40 +1,28 @@
-class VigenereCipheringMachine {
-    
+class VigenereCipheringMachine  {
+    constructor(typeMashine = true) {
+        this.typeMashine = typeMashine;
+    }
     encrypt(message, key) {
         if (typeof(message) !== 'string' || typeof(key) !== 'string') throw 'NO VALID ARGUMENTS';
-        
+        // console.log('this.encrypt.typeMashine1111: ', this.typeMashine);
         const alphabetArr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
 
         let messageArr = message.toUpperCase().split('');
-        // console.log('messageArr: ', messageArr);
 
-        // let longKey = key.repeat(Math.ceil(messageArr.length / key.length)).slice(0, messageArr.length);
+        if (this.typeMashine == false) {
+            messageArr.reverse();
+        }
+      
         let falseLongKey = key.repeat(Math.ceil(messageArr.length / key.length));
         let trueLenghtMessageArr = messageArr.filter(el => el.charCodeAt() >= 65 &&  el.charCodeAt() <= 90);
-        // console.log('trueLenghtMessageArr: ', trueLenghtMessageArr);
-
         let longKey = falseLongKey.slice(0, trueLenghtMessageArr.length);
-        // console.log('longKey: ', longKey);
-
         let longKeyArr = longKey.toUpperCase().split('');
-        // console.log('longKeyArr: ', longKeyArr);
-
-        // let messageArr = message.toUpperCase().split('');
-
-        // let longKey = key.repeat(Math.ceil(messageArr.length / key.length)).slice(0, messageArr.length);
-        // // console.log(longKey);
-
-        // let longKeyArr = longKey.split('');
-        // // messageArr = ["A", "T", "T", "A", "C", "K",  _______  "A", "T", _________ "D", "A", "W", "N"]
-        // //   longKeyArr = ["L", "E", "M", "O", "N", "L", "E", "M", "O", "N", "L", "E"]
 
         let codeMessageArr = messageArr.map(function(elem) {
             if (elem.charCodeAt() >= 65 &&  elem.charCodeAt() <= 90) {
-                // console.log('elem.charCodeAt(): ', elem, 'равно ', elem.charCodeAt());
                 return alphabetArr.findIndex(el => el == elem);
             }
             else {
-                // console.log('elem.charCodeAt(): ', elem, 'равно ', elem.charCodeAt());
                 return elem;
             }
         });
@@ -45,12 +33,10 @@ class VigenereCipheringMachine {
 
         let codeCipher = codeMessageArr.map(function(elem, index) {
             if (typeof(elem) == 'number') {
-                // console.log('elem.charCodeAt(): ', elem, 'равно ', elem.charCodeAt());
                 return elem + codeLongKeyArr[index];
             }
             else {
                 codeLongKeyArr.splice(index, 0, codeLongKeyArr[index]);
-                // console.log('elem.charCodeAt(): ', elem, 'равно ', elem.charCodeAt());
                 return elem;
             }
         });
@@ -68,34 +54,33 @@ class VigenereCipheringMachine {
                 return element;
             }
         });
-        console.log('cipher: ', cipher.join(''));
+        // console.log('Зашифровано: ', cipher.join(''));
+
+        if (this.typeMashine == false) {
+            cipher.reverse();
+            // console.log('Перевернуто зашифрованое: ', cipher.join(''));
+        }
+
         return cipher.join('');
 
     }
 
     decrypt(message, key) {
         if (typeof(message) !== 'string' || typeof(key) !== 'string') throw 'NO VALID ARGUMENTS';
-        // console.log('message:',message,"key:",key);
+        // console.log('message#############: ',message);
 
         const alphabetArr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
         let messageArr = message.toUpperCase().split('');
 
         let falseLongKey = key.repeat(Math.ceil(messageArr.length / key.length));
         let trueLenghtMessageArr = messageArr.filter(el => el.charCodeAt() >= 65 &&  el.charCodeAt() <= 90);
-        // console.log('trueLenghtMessageArr: ', trueLenghtMessageArr);
-
         let longKey = falseLongKey.slice(0, trueLenghtMessageArr.length);
-        // console.log('longKey: ', longKey);
-
         let longKeyArr = longKey.toUpperCase().split('');
-
         let codeMessageArr = messageArr.map(function(elem) {
             if (elem.charCodeAt() >= 65 &&  elem.charCodeAt() <= 90) {
-                // console.log('elem.charCodeAt(): ', elem, 'равно ', elem.charCodeAt());
                 return alphabetArr.findIndex(el => el == elem);
             }
             else {
-                // console.log('elem.charCodeAt(): ', elem, 'равно ', elem.charCodeAt());
                 return elem;
             }
         });
@@ -103,31 +88,22 @@ class VigenereCipheringMachine {
         let codeLongKeyArr = longKeyArr.map(function(elem) {
             return alphabetArr.findIndex(el => el == elem);
         });
-        // console.log('codeLongKeyArr до обработки: ', codeLongKeyArr);
-
         let codeDecipher = codeMessageArr.map(function(elem, index) {
             if (typeof(elem) == 'number') {
-                //  console.log('На индексе: ', index, ' стоит элемент ', elem, ' дешифратор для него равен ', codeLongKeyArr[index], ' после расшифровки его номер в алфавите будет ',  elem - codeLongKeyArr[index]);
                 return elem - codeLongKeyArr[index];
             }
             else {
                 codeLongKeyArr.splice(index, 0, codeLongKeyArr[index]);
-                // console.log('На индексе: ', index, 'попался не number. В codeLongKeyArr на индекс ', index, ' добавляю элемент ', codeLongKeyArr[index] );
                 return elem;
             }
         });
 
-        // console.log('codeDecipher: ', codeDecipher);
-        
-
         let deCipher = codeDecipher.map(element => {
             if (typeof(element) == 'number') {
                 if (element >= 0) {
-                    // console.log('>= 0', element);
                     return alphabetArr[element];
                 }
                 else {
-                    // console.log('< 0', element, Math.abs(element), alphabetArr.length - Math.abs(element));
                     return alphabetArr[alphabetArr.length - Math.abs(element)];
                 }
             }
@@ -135,7 +111,9 @@ class VigenereCipheringMachine {
                 return element;
             }
         });
-        console.log('deCipher: ', deCipher.join(''));
+        
+        // console.log('this.encrypt.typeMashine: ', this.typeMashine);
+
         return deCipher.join('');
 
     }
